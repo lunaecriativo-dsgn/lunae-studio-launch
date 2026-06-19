d6bf3c21aca24e7c2af91a45e49382e95fb4cbda
Lunae <lunae.criativo@gmail.com>
2026-06-19 18:37:51 -0300

    Configura deploy automatico github actions

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,11 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/dist/', // 👈 ADICIONE ESTA LINHA
+    //base: '/dist/', // 👈 ADICIONE ESTA LINHA
     plugins: [react(), tailwindcss()],
-    build: {
+    /*build: {
       outDir: 'public'
-            },
+            },*/
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
